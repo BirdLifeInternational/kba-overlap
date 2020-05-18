7be059b06c2aec5881a855855103556a287ceb20
ashley-simkins <47670555+ashley-simkins@users.noreply.github.com>
2020-05-18 15:39:44 +0100

    Updated codes and documentation
    R codes for the KBA-PA overlap analysis and generation of years for protected areas of unknown year of designation were updated with minor changes and the documentation of these codes was updated accordingly.

--- randomiser_2020_updated_clean.R
+++ randomiser_2020_updated_clean.R
@@ -19,7 +19,7 @@
 #TODO update wd to where you stored the Input data for R... files
 year_run <- format(Sys.Date(), "%Y")
 
-setwd (paste("C:/Users/Ashley.Simkins/Documents/SDG/KBA-PA overlap/KBA_PA_Overlap_rewritten/input tables", year_run))
+setwd (paste("C:/Users/Ashley.Simkins/OneDrive - BirdLife International/Documents/SDG/KBA-PA overlap/KBA_PA_Overlap_rewritten/input tables", year_run))
 #workd <- paste("C:/Users/Ashley.Simkins/Documents/SDG/KBA-PA overlap/KBA_PA_Overlap_rewritten/input tables", year_run)
 
 inout <- read.csv(paste("../in_out_files_", year_run, ".csv", sep = ""))
@@ -66,7 +66,7 @@ ranyear <- function(x, y, z, zx) {
         if (glb){
           out <- append(out, resample(as.numeric(x[y == y & zx != TRUE & z > 0]), 1, replace = T), after = length(out)) #use full protected area designation year list if <2 protected area years reported in the country
         } else if (reg){
-          out = append(out, resample(poolryears, 1, replace = TRUE), after = length(out))
+          out = append(out, resample(poolryears, 1, replace = T), after = length(out))
         }
       }
     }
@@ -90,20 +90,20 @@ stattotp <-function(x, y, id) {
 
 # cummulative mean percentage of IBAs that are protected
 # where x = protected and y = year and id = siteid
-statavep <-function(x, y, id) {
+statavep <- function(x, y, id) {
+#statavep(ibadat2$perprotected, output[,1], rownames(output)) {
   #yrs = seq(1900, 2016, by = 1)
   out <- as.vector(c())
-  for (i in 1:length(yrs))
-  {
-    temp = tapply(x[y <= yrs[i]], id[y <= yrs[i]], sum)
+  for (i in 1:length(yrs)){
+    temp = tapply(x[y <= yrs[i]], id[y <= yrs[i]], sum) #sums % coverages within each  sites up to year i
     if (glb){
-      out = append(out, sum(temp/100), after = length(out))
+      out = append(out, sum(temp/100), after = length(out)) #sums % coverage across all sites in this country/region up to year i
     }
     if (reg){
-      if (length(temp)>0){
+      if (length(temp) > 0){
         out = append(out, sum(temp/100), after = length(out))
       }
-      if (length(temp)==0){
+      if (length(temp) == 0){
         out = append(out, 0, after = length(out))
       }
     }  
@@ -159,7 +159,8 @@ calcvals2 = function(x,y) {
 ### Part 2 - run randomisation for each regional grouping in turn ###############
 
 
-for (w in 1:nrow(inout)){ #loops through input files (using file names from inout csv file)
+for (w in 5:nrow(inout)){ #loops through input files (using file names from inout csv file)
+#for (w in 3:4){
   #w=13 #for CMS regions only
   #w=12 #for new World_bank only
   #w=10
@@ -182,136 +183,144 @@ for (w in 1:nrow(inout)){ #loops through input files (using file names from inou
   #outfile2 = "Output data for R Region KBAs.csv"
   
   #if (glb) {
+  
+  # NOTE: Outfiles will be overwritten, including those for regions.
+  ####################
+  
+  ibadat1 <-  read.csv(infile, header = TRUE, sep = ",", quote="\"", dec=".", comment.char="", as.is = TRUE) # read in data
+  
+  outputfile1 <- as.character(inout$outputfile1[w])
+  
+  poolryears <- ibadat1$year[ibadat1$random_year == FALSE & ibadat1$year > 0] #create a list of years that were not randomly assigned, to use if a country has no random years #note this uses ibadat1 as needs to not only look at the system subset, but from the protected areas in the country as a whole
+  
+  
+  # if (glb){
+  #   outputfile1 <- "Output data for R Country KBAs"
+  # }
+  
+  for (k in ssets2run){
+    #k=1
+    sset <- subsets[k]
+    if (sset == 'all') { #(k == 1) {
+      ibadat <- ibadat1
+      outfile <- paste(inout$outputfile1[w], "_", sset, ".csv", sep = "")
+    }
+    #ibadat <- ibadat1 #read in data from input file
     
-    # NOTE: Outfiles will be overwritten, including those for regions.
-    ####################
+    if (sset != 'all') { #(k > 1) {
+      kbasi <- tabmf$SitRecID[tabmf[,which(names(tabmf) == sset)] == 1]
+      ibadat <- ibadat1[ibadat1$siteid %in% kbasi,]
+      outfile <- paste(inout$outputfile1[w], "_", sset, ".csv", sep = "")
+    }
     
-    ibadat1 <-  read.csv(infile, header = TRUE, sep = ",", quote="\"", dec=".", comment.char="", as.is = TRUE) # read in data
+    #outfile <- paste(inout$outputfile1[w], "_", sset, ".csv", sep = "")
     
-    outputfile1 <- as.character(inout$outputfile1[w])
+    head(ibadat)
+    regs <- unique(ibadat$region)
     
     # if (glb){
-    #   outputfile1 <- "Output data for R Country KBAs"
+    #   regs <- "global"
     # }
     
-    for (k in ssets2run){
-      #k=1
-      sset <- subsets[k]
-      if (k == 1) {
-        ibadat <- ibadat1
-        outfile <- paste(inout$outputfile1[w], ".csv", sep="")
+    regs
+    length(regs)
+    
+    
+    
+    regres <- data.frame()
+    for (z in 1:length(regs)){
+      #z=152
+      #z=which(regs=="Germany")
+      #if (reg){
+      z
+      regs[z]
+      #ibadat <- ibadat1 #removed as didn't think this was necessary? Check
+      ibadat2 <- ibadat[ibadat$region == regs[z],]
+      #ibadat <- ibadat2
+      #}
+      
+      #attach (ibadat2)
+      
+      #attach (ibadat)
+      n <- length(ibadat2$siteid)
+      ibaname <- unique(ibadat2$siteid)
+      m <-  length(unique(ibadat2$siteid))
+      print(paste("Number of KBAs being analysed ", m," with ", n, " sites.", sep = ""))
+      #poolryears <- ibadat2$year[ibadat2$random_year == FALSE] #create a list of years that were not randomly assigned, to use if a country has no random years
+      ####################
+      
+      ###############################
+      # start data randomization
+      ##############################
+      
+      output <- mkdata(ibadat2$perprotected, ibadat2$country, ibadat2$year, ibadat2$random_year, ibadat2$siteid, j) #creates a list of years to make the error bars from (j repetitions so in this case a list of 100 possible years) #rows are the siteIDs, columns are the repetitions
+      
+      # calculate the results   ## % of PAs fully covered
+      alldata <- calcvals(ibadat2$perprotected, output) #creates matrix of possible values for all repetitions for all years, where columns are different years and rows are the different repetitions based on the years randomly determined above - this is now country level data
+      
+      res <- matrix(data = NA, ncol = 7, nrow = length(yrs), dimnames = list(c(seq(1,length(yrs), by =1)),c("year", "95CIlowCount","95CImidCount","95CIhiCount", "95CIlowPercentage", "95CImidPercentage", "95CIhiPercentage")))
+      res[,1] <- yrs
+      for (i in 1:length(yrs)){
+        res[i,2] <- quantile(alldata[,i], probs = c(0.025))
+        res[i,3] <- quantile(alldata[,i], probs = c(0.5))
+        res[i,4] <- quantile(alldata[,i], probs = c(0.975))
+        print(i)
       }
-      if (k > 1) {
-        kbasi <- tabmf$SitRecID[tabmf[,which(names(tabmf)==sset)]==1]
-        ibadat <- ibadat1[ibadat1$siteid%in%kbasi,]
-        outfile <- paste(inout$outputfile1[w], "_", sset, ".csv", sep="")
+      res[,5] <- res[,2]/m *100
+      res[,6] <- res[,3]/m *100
+      res[,7] <- res[,4]/m *100
+      
+      res2 <- data.frame(res)
+      names(res2) = c("year", "95CIlowCount","95CImidCount","95CIhiCount", "95CIlowPercentage", "95CImidPercentage", "95CIhiPercentage")
+      
+      #write.table(res, file = outfile, append = FALSE, quote = TRUE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = FALSE)
+      
+      
+      # calculate the results   ## % ok KBAs covered
+      alldata <- calcvals2(ibadat2$perprotected, output)
+      
+      res <- matrix(data = NA, ncol = 7, nrow = length(yrs), dimnames = list(c(seq(1,length(yrs), by =1)),c("year", "95CIlowCount","95CImidCount","95CIhiCount", "95CIlowPercentage_Area", "95CImidPercentage_Area", "95CIhiPercentage_Area")))
+      res[,1] <- yrs
+      for (i in 1:length(yrs)){
+        #i=23
+        res[i,2] <- quantile(alldata[,i], probs = c(0.025))
+        res[i,3] <- quantile(alldata[,i], probs = c(0.5))
+        res[i,4] <- quantile(alldata[,i], probs = c(0.975))
+        print(i)
+      }
+      res[,5] <- res[,2]/m *100
+      res[,6] <- res[,3]/m *100
+      res[,7] <- res[,4]/m *100
+      
+      res2$CI95lowPercentage_Area <- res[,5]
+      res2$CI95midPercentage_Area <- res[,6]
+      res2$CI95hiPercentage_Area <- res[,7]
+      
+      res2$region <- regs[z]
+      res2$code <- inout$code[w]
+      res2$sset <- sset
+      
+      if (glb){
+        write.csv(res2, file = outfile, row.names = F)
       }
       
-      head(ibadat)
-      regs <- unique(ibadat$region)
+      if (reg){
+        regres <- rbind(regres, res2)
+      }
       
-      # if (glb){
-      #   regs <- "global"
-      # }
+      #detach(ibadat)
       
-      regs
-      length(regs)
       
-
-      
-      regres <- data.frame()
-      for (z in 1:length(regs)){
-        #z=152
-        #z=which(regs=="Germany")
-        #if (reg){
-        z
-        ibadat <- ibadat1
-        ibadat2 <- ibadat[ibadat$region == regs[z],]
-        #ibadat <- ibadat2
-        #}
-        
-        #attach (ibadat2)
-      
-        #attach (ibadat)
-        n <- length(ibadat2$siteid)
-        ibaname <- unique(ibadat2$siteid)
-        m <-  length(unique(ibadat2$siteid))
-        print(paste("Number of KBAs being analysed ", m," with ",n, " sites.", sep = ""))
-        poolryears <- ibadat2$year[ibadat2$random_year == FALSE] #create a list of years that were not randomly assigned, to use if a country has no random years
-        ####################
-
-        ###############################
-        # start data randomization
-        ##############################
-
-        output <- mkdata(ibadat2$perprotected, ibadat2$country, ibadat2$year, ibadat2$random_year, ibadat2$siteid, j) #creates a list of years to make the error bars from (j repetitions so in this case a list of 100 possible years)
-        
-        # calculate the results   ## % of PAs fully covered
-        alldata <- calcvals(ibadat2$perprotected, output)
-        
-        res <- matrix(data = NA, ncol = 7, nrow = length(yrs), dimnames = list(c(seq(1,length(yrs), by =1)),c("year", "95CIlowCount","95CImidCount","95CIhiCount", "95CIlowPercentage", "95CImidPercentage", "95CIhiPercentage")))
-        res[,1] <- yrs
-        for (i in 1:length(yrs)){
-          res[i,2] <- quantile(alldata[,i], probs = c(0.025))
-          res[i,3] <- quantile(alldata[,i], probs = c(0.5))
-          res[i,4] <- quantile(alldata[,i], probs = c(0.975))
-          print(i)
-        }
-        res[,5] <- res[,2]/m *100
-        res[,6] <- res[,3]/m *100
-        res[,7] <- res[,4]/m *100
-        
-        res2 <- data.frame(res)
-        names(res2) = c("year", "95CIlowCount","95CImidCount","95CIhiCount", "95CIlowPercentage", "95CImidPercentage", "95CIhiPercentage")
-        
-        #write.table(res, file = outfile, append = FALSE, quote = TRUE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = FALSE)
-        
-        
-        # calculate the results   ## % ok KBAs covered
-        alldata <- calcvals2(ibadat2$perprotected, output)
-        
-        res <- matrix(data = NA, ncol = 7, nrow = length(yrs), dimnames = list(c(seq(1,length(yrs), by =1)),c("year", "95CIlowCount","95CImidCount","95CIhiCount", "95CIlowPercentage_Area", "95CImidPercentage_Area", "95CIhiPercentage_Area")))
-        res[,1] <- yrs
-        for (i in 1:length(yrs)){
-          #i=23
-          res[i,2] <- quantile(alldata[,i], probs = c(0.025))
-          res[i,3] <- quantile(alldata[,i], probs = c(0.5))
-          res[i,4] <- quantile(alldata[,i], probs = c(0.975))
-          print(i)
-        }
-        res[,5] <- res[,2]/m *100
-        res[,6] <- res[,3]/m *100
-        res[,7] <- res[,4]/m *100
-        
-        res2$CI95lowPercentage_Area <- res[,5]
-        res2$CI95midPercentage_Area <- res[,6]
-        res2$CI95hiPercentage_Area <- res[,7]
-        
-        res2$region <- regs[z]
-        res2$code <- inout$code[w]
-        res2$sset <- sset
-        
-        if (glb){
-          write.csv(res2, file = outfile, row.names = F)
-        }
-        
-        if (reg){
-          regres <- rbind(regres, res2)
-        }
-        
-        #detach(ibadat)
-       
-        
-      } #ends loop of regs
-        if (reg){
-          str(regres)
-          unique(regres$region)
-          length(unique(regres$region))
-          tail(regres)
-          write.table(regres, file = outfile, append = FALSE, quote = TRUE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = FALSE)
-        } 
-
-      } #ends loop of subsets (marine, Freshwater, etc)
+    } #ends loop of regs
+    if (reg){
+      str(regres)
+      unique(regres$region)
+      length(unique(regres$region))
+      tail(regres)
+      write.table(regres, file = outfile, append = FALSE, quote = TRUE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = FALSE)
+    } 
+    
+  } #ends loop of subsets (marine, Freshwater, etc)
   # end script
 } ## ends the loop of input files
 ##############
